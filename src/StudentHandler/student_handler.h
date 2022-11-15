08d6c275b2225d078038404a536dd84714fe5119
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-15 03:07:48 -0500

    Create student handler object header file

---
+++ src/StudentHandler/student_handler.h
@@ -0,0 +1,11 @@
+#ifndef STUDENT_HANDLER_H // STUDENT_HANDLER_H
+#define STUDENT_HANDLER_H
+#include <iostream>
+
+void setStudentId(int id);
+void setStudentName(char name[]);
+int getStudentId();
+char* getStudentName();
+void printStudentDetails();
+
+#endif // STUDENT_HANDLER_H
