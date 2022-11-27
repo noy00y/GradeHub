f83fd83e64ba77d43ce918117e9789d3f14b5ba4
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-27 05:50:07 -0500

    Create student handler header

--- src/StudentHandler/student_handler.h
+++ src/StudentHandler/student_handler.h
@@ -1,11 +1,38 @@
-#ifndef STUDENT_HANDLER_H // STUDENT_HANDLER_H
+#ifndef STUDENT_HANDLER_H /* STUDENT_HANDLER_H */
 #define STUDENT_HANDLER_H
+
 #include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
 
-void setStudentId(int id);
-void setStudentName(char name[]);
-int getStudentId();
-char* getStudentName();
-void printStudentDetails();
+#include <student_handler.h>
+#include <course_handler.h>
+#include <utils.h>
 
-#endif // STUDENT_HANDLER_H
+using std::cout;
+using std::string;
+using std::vector;
+using std::fstream;
+using std::stringstream;
+using std::ios;
+
+class StudentHandler {
+     private:
+          string name;
+          int studentID;
+          vector<CourseHandler> courses;
+          int num_courses;
+
+     public:
+          StudentHandler(string studentName, int ID) { return };
+          StudentHandler(string data) { return };
+          void add_course(string input) { return };
+          void print() { return };
+
+}
+
+#endif /* STUDENT_HANDLER_H */
