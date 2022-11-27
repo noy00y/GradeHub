eef61a23604e05b769ed2ada8084a6ef311d3aa9
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-27 05:51:21 -0500

    Create course handler header

---
+++ src/CourseHandler/course_handler.h
@@ -0,0 +1,37 @@
+#ifndef COURSE_HANDLER_H /* COURSE_HANDLER_H */
+#define COURSE_HANDLER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cstdio>
+#include <sstream>
+
+#include <course_handler.h>
+#include <utils.h>
+
+using std::cout;
+using std::string;
+using std::vector;
+using std::stringstream;
+using std::to_string;
+
+class CourseHandler {
+     private:
+          int studentID;
+          int num_scores; 
+          string finalScore; 
+          vector<float> scores;
+
+     public:
+          string code;
+
+          CourseHandler(string data);
+
+          void print() { return };
+          bool taken_by(int given_ID) { return given_ID == studentID };
+};
+
+#endif /* COURSE_HANDLER_H */
