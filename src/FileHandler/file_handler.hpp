7ca7f60ea11aa0087fa3413e4b85ed39cc5d0533
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-27 07:05:37 -0500

    Update file extension for file handler

---
+++ src/FileHandler/file_handler.hpp
@@ -0,0 +1,21 @@
+#ifndef FILE_HANDLER_H // FILE_HANDLER_H
+#define FILE_HANDLER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cstdio>
+// using namespace std;
+
+// class FileHandler{
+// public:
+//     string filename;
+// public:
+//     FileHandler();
+//     vector<vector<string>> readlines();
+//     void print();
+// };
+
+#endif // FILE_HANDLER_H
