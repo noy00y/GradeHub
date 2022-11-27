ed9a4c759ee44abb7c20557fbcf0303ea2ac72b3
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-27 05:49:28 -0500

    Refactor student object and methods

--- src/StudentHandler/student_handler.cpp
+++ src/StudentHandler/student_handler.cpp
@@ -1,30 +1,112 @@
-#include <iostream>
 #include <student_handler.h>
+#include <course_handler.h>
+#include <utils.h>
 
+/*
+* 
+* Student Handler Object
+* ==================================
+* Description: 
+*     This object is used to handle 
+*     each student & their courses
+* ==================================
+*/
 class StudentHandler {
+     /*
+     * 
+     * Private attributes
+     *  - name (string) : the name of the student
+     *  - studentID (int) : the ID of the student
+     *  - courses (vector<CourseHandler>) : the courses of the student 
+     *  - num_courses (int) : the number of courses
+     * 
+     */  
+     private: 
+          string name;
+          int studentID;
+          vector<CourseHandler> courses;
+          int num_courses; 
+
+     /*
+     * 
+     * Public attributes & methods
+     *  - StudentHandler (constructor) : the constructor of the object for a given name and ID
+     *  - StudentHandler (constructor) : the constructor of the object for a given data string
+     *  - add_course (method) : adds a course to the student
+     *  - print (method) : prints the student information
+     * 
+     */   
      public:
-          int student_id;
-          char student_name[50];
-
-          void setStudentId(int id) {
-               student_id = id;
+          StudentHandler(string studentName, int ID) { 
+               name = studentName;
+               studentID = ID;
+               num_courses = 0;
+               return;
           }
 
-          void setStudentName(char name[]) {
-               strcpy(student_name, name);
+          StudentHandler(string data) {
+               name = "None";
+               studentID = 0;
+               num_courses = 0;
+
+               remove(data,',');
+               stringstream strIn(data);
+               string line;
+
+               while (!strIn.fail()) {
+                    strIn >> line;
+                    if (line.size() != 0) {
+                         if (isnum(line)) {
+                              studentID=stoi(line);
+                         } else {
+                              if (name=="None") {
+                                   name=line;
+                              } else {
+                                   if (!strIn.fail()) {
+                                        strIn >> line;
+                                        name.append(" ");
+                                        name.append(line);
+                                   }
+                              }
+                         }
+                    }
+               }
           }
 
-          int getStudentId() {
-               return student_id;
-          }
+          /*
+          * 
+          * Adds a course object to the student's list of courses.
+          * Returns nothing.
+          * 
+          */
+          void add_course(string input) {
+               remove(input,',');
+               CourseHandler course(input);
 
-          char* getStudentName() {
-               return student_name;
-          }
+               if (course.takenBy(studentID)) {
+                    courses.push_back(course);
+                    num_courses++;
+               }
 
-          void printStudentDetails() {
-               std::cout << "Student ID: " << student_id << std::endl;
-               std::cout << "Student Name: " << student_name << std::endl;
+               return;
           }
+          
+          /*
+          * 
+          * Prints the data and calls print on the course objects when called.    
+          * Returns nothing.
+          * 
+          */
+          void print() {
+               cout << "Student Name: " << name << "\n";
+               cout << "Student ID: " << studentID << "\n";
+               cout << "Number of courses: " << num_courses << "\n";
+               cout << "Courses: " << "\n";
+
+               for (int i = 0; i < num_courses; i++) {
+                    courses[i].print();
+               }    
+
+               return;
+          }     
 };
-
