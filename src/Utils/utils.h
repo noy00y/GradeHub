52907705679451d83a814493302db72378718b44
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-27 05:52:20 -0500

    Create utility functions header

---
+++ src/Utils/utils.h
@@ -0,0 +1,20 @@
+#ifndef UTILS_HEADER_H /* UTILS_HEADER_H */
+#define UTILS_HEADER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::fstream;
+using std::stringstream;
+using std::ios;
+
+void remove(string &input, char to_replace);
+bool isnum(string line);
+vector<string> readlines(string filename);
+
+#endif /* UTILS_HEADER_H */
